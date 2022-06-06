11e18fb390144c06614e2cc8b2dcb6aed7453707
ornaldo_ <46441190+gjeornaldo@users.noreply.github.com>
2022-06-06 12:41:29 +0200

    Update 5-prepare_viz_for_dashboard.R

--- 5-prepare_viz_for_dashboard.R
+++ 5-prepare_viz_for_dashboard.R
@@ -1,3 +1,6 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, stringi)
+
 ## In order to upload the visualizations for the dashboard we need to remove spaces and commas, and uniform the encoding 
 
 # For the htmls
@@ -19,7 +22,7 @@ file.rename(list.files(here("visualizations", "density_plots"), full.names = T),
             str_replace_all(list.files(here("visualizations", "density_plots"), full.names = T), ",", ""))
 
 file.rename(list.files(here("visualizations", "density_plots"), full.names = T), 
-            stringi::stri_trans_general(list.files(here("visualizations", "density_plots"), full.names = T), "Latin-ASCII"))
+            stri_trans_general(list.files(here("visualizations", "density_plots"), full.names = T), "Latin-ASCII"))
 
 
 ## We also need to create a file for the dropdown selector
@@ -29,5 +32,5 @@ selector <- lau_matched %>%
   unique() %>% 
   arrange(country_id, region) %>% 
   mutate(region = str_replace_all(region, ",", ""),
-         region = stringi::stri_trans_general(region, "Latin-ASCII")) %>% 
+         region = stri_trans_general(region, "Latin-ASCII")) %>% 
   write_csv("selector_az.csv")
